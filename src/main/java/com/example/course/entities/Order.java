be55beb0ec9f97d21620f0a03ae21e01b9846864
Dione Cezar <dione_cezar@hotmail.com>
2019-09-14 11:58:30 -0300

    Subtotal & Total methods

--- src/main/java/com/example/course/entities/Order.java
+++ src/main/java/com/example/course/entities/Order.java
@@ -100,6 +100,16 @@ public class Order implements Serializable {
 	public Set<OrderItem> getItems() {
 		return items;
 	}
+	
+	public Double getTotal() {
+		double sum = 0d;
+		
+		for (OrderItem x : items) {
+			sum += x.getSubTotal();
+		}
+		
+		return sum;
+	}
 
 	@Override
 	public int hashCode() {
