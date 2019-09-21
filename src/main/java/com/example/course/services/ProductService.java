7a453161cf55b652af97c4d50ff37cef9b188984
Dione Cezar <dione_cezar@hotmail.com>
2019-09-21 15:55:49 -0300

    Product insert

--- src/main/java/com/example/course/services/ProductService.java
+++ src/main/java/com/example/course/services/ProductService.java
@@ -6,9 +6,14 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.example.course.dto.CategoryDTO;
+import com.example.course.dto.ProductCategoriesDTO;
 import com.example.course.dto.ProductDTO;
+import com.example.course.entities.Category;
 import com.example.course.entities.Product;
+import com.example.course.repositories.CategoryRepository;
 import com.example.course.repositories.ProductRepository;
 import com.example.course.services.exceptions.ResourceNotFoundException;
 
@@ -17,6 +22,9 @@ public class ProductService {
 	
 	@Autowired
 	private ProductRepository repository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 	
 	public List<ProductDTO> findAll() {
 		List<Product> list = repository.findAll();
@@ -28,6 +36,22 @@ public class ProductService {
 		Product entity = obj.orElseThrow(() -> new ResourceNotFoundException(id));
 		
 		return new ProductDTO(entity);
+	}
+	
+	@Transactional
+	public ProductDTO insert(ProductCategoriesDTO dto) {
+		Product entity = dto.toEntity();
+		setProductCategories(entity, dto.getCategories());
+		entity = repository.save(entity);
+		return new ProductDTO(entity);
+	}
+
+	private void setProductCategories(Product entity, List<CategoryDTO> categories) {
+		entity.getCategories().clear();
+		for (CategoryDTO dto : categories) {
+			Category category = categoryRepository.getOne(dto.getId());
+			entity.getCategories().add(category);
+		}
 	}
 
 }
