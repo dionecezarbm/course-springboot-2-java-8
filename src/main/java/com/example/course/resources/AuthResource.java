c70256d523f56a6dccbe56e816b2ac9ebc43c41e
Dione Cezar <dione_cezar@hotmail.com>
2019-11-07 22:39:49 -0200

    Refresh token

--- src/main/java/com/example/course/resources/AuthResource.java
+++ src/main/java/com/example/course/resources/AuthResource.java
@@ -23,5 +23,11 @@ public class AuthResource {
 		TokenDTO tokenDTO = service.authenticate(dto);
 		return ResponseEntity.ok().body(tokenDTO);
 	}
+	
+	@PostMapping("/refresh")
+	public ResponseEntity<TokenDTO> refresh(CredentialsDTO dto) {
+		TokenDTO tokenDTO = service.refreshToken();
+		return ResponseEntity.ok().body(tokenDTO);
+	}
 
 }
